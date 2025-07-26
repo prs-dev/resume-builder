6b4f9fdc1e77c98ae9570f6590d9054249bfe82f
prs-dev <pratyushsri.25@gmail.com>
2025-07-26 22:19:06 +0530

    html2pdf.js integrated

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -1,9 +1,12 @@
 import { MdEmail } from "react-icons/md";
 import { MdLocalPhone } from "react-icons/md";
+import { useRef } from "react";
+import html2pdf from 'html2pdf.js/dist/html2pdf.min'
 
-const Preview = ({data}) => {
-  console.log("test",data)
-  console.log("test",Object.values(data).every(i => i === ''))
+
+const Preview = ({ data }) => {
+  // console.log("test",data)
+  // console.log("test",Object.values(data).every(i => i === ''))
   const dateFormatter = (date) => {
     // const options = {month: "long", year: "numeric"}
     // const formatter = new Intl.DateTimeFormat('en-US', options)
@@ -13,59 +16,82 @@ const Preview = ({data}) => {
     console.log(month, year)
     return `${month} ${year}`
   }
+  const contentRef = useRef(null)
+  console.log("ref", contentRef)
+  const generatePdf = () => {
+  if (contentRef.current) {
+    console.log("i am here")
+    const opt = {
+      margin: 0.5,
+      filename: 'resume.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 }, // better quality
+      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    html2pdf().set(opt).from(contentRef.current).save("test.pdf");
+    console.log("everything run")
+  }
+};
+
+  // console.log(html2pdf)
   return (
-    <div className='p-2 flex gap-4 flex-col '>
-      {Object.values(data).every(i => i === '') && <h1 className='text-gray-400'>Preview</h1>}
-      <h1 className='text-4xl'>{data?.name}</h1>
-      <p className='text-sm'>{data?.title}</p>
-      <div className='flex items-center justify-between shadow-sm p-2 rounded-sm'>
-        <div className="flex gap-2 items-center">
-          {data.email && <MdEmail />}
-          <p className='text-sm'>{data?.email}</p>
+    <>
+    <div className='p-2 flex gap-4 flex-col' ref={contentRef}>
+      {/* {Object.values(data).every(i => i === '') && <h1 className='text-gray-400'>Preview</h1>} */}
+      
+        <h1 className='text-4xl'>{data?.name}</h1>
+        <p className='text-sm'>{data?.title}</p>
+        <div className='flex items-center justify-between shadow-sm p-2 rounded-sm'>
+          <div className="flex gap-2 items-center">
+            {data.email && <MdEmail />}
+            <p className='text-sm'>{data?.email}</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            {data.phone && <MdLocalPhone />}
+            <p className='text-sm font-bold'>{data?.phone}</p>
+          </div>
         </div>
-        <div className="flex gap-2 items-center">
-          {data.phone && <MdLocalPhone />}
-          <p className='text-sm font-bold'>{data?.phone}</p>
+        <div>
+          <h2 className="text-xl my-2 underline py-2">Education</h2>
+          <ul>
+            {data?.education?.map(item => <li className="list-disc flex flex-col text-sm gap-2">
+              <div>School: {item.school}</div>
+              <div>Degree: {item.degree}</div>
+              <div>Duration: {item.start} - {item.end}</div>
+              <hr className="my-2" />
+            </li>)}
+          </ul>
         </div>
-      </div>
-      <div>
-        <h2 className="text-xl my-2 underline py-2">Education</h2>
-        <ul>
-          {data?.education?.map(item => <li className="list-disc flex flex-col text-sm gap-2">
-            <div>School: {item.school}</div>
-            <div>Degree: {item.degree}</div>
-            <div>Duration: {item.start} - {item.end}</div>
-            <hr className="my-2"/>
-          </li>)}
-        </ul>
-      </div>
-      <div>
-        <h2 className="text-xl my-2 underline py-2">Skills</h2>
-        <ul className="p-2 text-sm">
-          {data?.skills?.map(item => <li className="list-decimal">{item}</li>)}
-        </ul>
-      </div>
-      <div>
-        <h2 className="text-xl my-2 underline py-2">Experience</h2>
-        <ul className="list-disc p-2 flex flex-col gap-2">
-          {
-          data?.experience?.map(item => (
-            <li>
-              <div>
-                <span>{item?.role}</span> -- <span>{item?.company}</span>
-              </div>
-              <div>
-                <span>{dateFormatter(item?.start)}</span> - <span>{dateFormatter(item?.end)}</span>
-              </div>
-              <div>
-                <p>{item?.description}</p>
-              </div>
-            </li>
-          ))
-        }
-        </ul>
-      </div>
+        <div>
+          <h2 className="text-xl my-2 underline py-2">Skills</h2>
+          <ul className="p-2 text-sm">
+            {data?.skills?.map(item => <li className="list-decimal">{item}</li>)}
+          </ul>
+        </div>
+        <div>
+          <h2 className="text-xl my-2 underline py-2">Experience</h2>
+          <ul className="list-disc p-2 flex flex-col gap-2">
+            {
+              data?.experience?.map(item => (
+                <li>
+                  <div>
+                    <span>{item?.role}</span> -- <span>{item?.company}</span>
+                  </div>
+                  <div>
+                    <span>{dateFormatter(item?.start)}</span> - <span>{dateFormatter(item?.end)}</span>
+                  </div>
+                  <div>
+                    <p>{item?.description}</p>
+                  </div>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
     </div>
+      <button onClick={generatePdf}>print</button>
+      </>
   )
 }
 
